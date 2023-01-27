b528e6fb065a92d16cc5479766a4b26506cc8a7b
Severin FERARD <sferard@student.42.fr>
2023-01-27 23:26:55 +0100

    improved typescript typing in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,18 +2,50 @@ import { DetailedReactHTMLElement } from "react";
 import "react-native";
 import { ImageStyle, TextStyle, ViewStyle } from "react-native";
 
+export type StyleProperty =
+  | ViewStyle
+  | TextStyle
+  | ImageStyle
+  | { [k: string]: ViewStyle | TextStyle | ImageStyle };
+
 export type NamedStyles<T> = {
-  [P in keyof T]: ViewStyle | TextStyle | ImageStyle;
+  [P in keyof T]: StyleProperty;
 };
+
+type BaseStyle<UserStyles> = {
+  [Element in keyof UserStyles]: {
+    [Property in keyof UserStyles[Element] as Property extends `@media${string}`
+      ? never
+      : Property]: UserStyles[Element][Property];
+  };
+};
+
+type ResponsiveStyle<UserStyles> = {
+  [Element in keyof UserStyles]: {
+    [P in {
+      [Property in keyof UserStyles[Element]]: Property extends `@media${string}`
+        ? {
+            [QueryProperty in keyof UserStyles[Element][Property]]: [
+              QueryProperty,
+              UserStyles[Element][Property][QueryProperty]
+            ];
+          }[keyof UserStyles[Element][Property]]
+        : never;
+    }[keyof UserStyles[Element]] as P[0]]?: P[1];
+  };
+};
+
+type ComputedStyle<UserStyles> = BaseStyle<UserStyles> &
+  ResponsiveStyle<UserStyles>;
 
 export declare function create<
   UserStyles extends NamedStyles<UserStyles> | NamedStyles<any>
 >(
   styles: UserStyles | NamedStyles<UserStyles>
 ): {
-  styles: UserStyles;
   fullStyles: UserStyles;
   ids: Record<keyof UserStyles, string>;
+  styles: ComputedStyle<UserStyles>;
 };
 
 export declare function process<
